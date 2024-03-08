1422027161a43bf2886eb729903b198727b0754e
Mario Espinoza <mario221649@gmail.com>
2024-03-08 11:21:13 -0700

    Decaracion de Variables

--- src/curso_goland_platzi/src/main.go
+++ src/curso_goland_platzi/src/main.go
@@ -3,5 +3,35 @@ package main
 import "fmt"
 
 func main() {
+
 	fmt.Println("Hola Mundo")
+
+	//Declaración de variables
+	const pi float64 = 3.14
+
+	const pi2 float64 = 3.15
+
+	fmt.Println("pi: ", pi)
+	fmt.Println("pi2: ", pi2)
+
+	//Declaración de Variables Enteras
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	//Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	//Area de un Cuadrado
+
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println("Area del Cuadrado: ", areaCuadrado)
 }
